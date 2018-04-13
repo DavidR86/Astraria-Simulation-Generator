df3b4800092daeecc9474086bcd6b0f75a93efc7
2019 - ROCKENZAHN GALLEGOS, DAVID THOMAS <davrockenzahn19@David-Rockenzahn-Gallegos.local>
2018-04-13 12:44:09 -0500

    added functions and buttons of backup screen controller

--- src/sample/controllers/BackupScreenController.java
+++ src/sample/controllers/BackupScreenController.java
@@ -7,5 +7,40 @@ package sample.controllers;/*
 
 */
 
+import javafx.event.ActionEvent;
+import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+import sample.Main;
+
 public class BackupScreenController {
+    public TextField backupSimPath;
+    public TextField backupFilePath;
+    public Button backupSimPathButton;
+    public Button backupFilePathButton;
+    public TextField backupDurationField;
+    public ComboBox backupDurationComboBox;
+    public TextArea backupStatusField;
+    public Button cancelButton;
+    public Button startButton;
+    public Main mainClass;
+
+    public void setBackupSimPath(ActionEvent actionEvent) {
+    }
+
+    public void setOutputFilePath(ActionEvent actionEvent) {
+    }
+
+    public void goBack(ActionEvent actionEvent) {
+        mainClass.ChangeToTitleScreen();
+    }
+
+    public void startSimulation(ActionEvent actionEvent) {
+
+    }
+
+    public void setMain (Main main){
+        mainClass = main;
+    }
 }
